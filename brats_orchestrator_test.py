17e39a9cba14af0e57500ce837c926d792b61056
gonzaloemr <gonzalo.mosquerarojas2000@gmail.com>
2026-02-19 17:30:19 +0100

    test update

--- brats_orchestrator_test.py
+++ brats_orchestrator_test.py
@@ -18,13 +18,13 @@ def run_brats_orchestrator(input_dir, single_patient = False):
             
             patient = Path(input_dir)
             preprocessed_dir = patient / (patient.name + "_brainles") / "raw_bet"
-            segmentation_dir = patient / (patient.name + "_brainles") / "segmentation"
+            segmentation_dir = patient / (patient.name + "_brainles") / "segmentation_bt_redo"
             segmenter.infer_single(
                 t1c=preprocessed_dir.joinpath(patient.name + "_t1c_bet.nii.gz"),
                 t1n=preprocessed_dir.joinpath(patient.name + "_t1_bet.nii.gz"),
                 t2f=preprocessed_dir.joinpath(patient.name + "_fla_bet.nii.gz"),
                 t2w=preprocessed_dir.joinpath(patient.name + "_t2_bet.nii.gz"),
-                output_file=segmentation_dir.joinpath(f"brats_orchestrator_segmentation_{idx+1}.nii.gz"),
+                output_file=segmentation_dir.joinpath(f"bo_{idx+1}.nii.gz"),
             )
             
             return
@@ -32,7 +32,7 @@ def run_brats_orchestrator(input_dir, single_patient = False):
         for patient in Path(input_dir).iterdir():
             
             preprocessed_dir = patient / (patient.name + "_brainles") / "raw_bet"
-            segmentation_dir = patient / (patient.name + "_brainles") / "segmentation"
+            segmentation_dir = patient / (patient.name + "_brainles") / "segmentation_bo_redo"
             segmenter.infer_single(
                 t1c=preprocessed_dir.joinpath(patient.name + "_t1c_bet.nii.gz"),
                 t1n=preprocessed_dir.joinpath(patient.name + "_t1_bet.nii.gz"),
@@ -43,7 +43,7 @@ def run_brats_orchestrator(input_dir, single_patient = False):
 
 if __name__ == "__main__":
 
-    input_dir = "/data/glioma_data/skull_stripped_scans_2/"
+    input_dir = "/data/glioma_data/skull_stripped_scans_2/patients"
     run_brats_orchestrator(input_dir)
 
     # models_list = [AdultGliomaPreTreatmentAlgorithms.BraTS23_1,
